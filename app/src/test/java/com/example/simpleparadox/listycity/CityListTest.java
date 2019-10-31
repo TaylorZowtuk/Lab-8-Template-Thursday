434d5f2c887ded5e646db16de3c29dd7dae1bd8d
TaylorZowtuk <zowtukt@gmail.com>
2019-10-31 17:43:56 -0600

    Testing travis failing integration

--- app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
+++ app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
@@ -93,4 +93,10 @@ class CityListTest {
 
         assertEquals(1, cityList.countCities());
     }
+
+    @Test
+    void testReturnOne() {
+        // method doesnt exist so travis integration should fail
+        assertEquals(1, returnOne());
+    }
 }
